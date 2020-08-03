5fa993dbed8206c3dfc50ad6395d238a77661755
Scionwest <scionwest+github@gmail.com>
2020-08-03 05:50:34 +0000

    Updated template to replace hard-coded focusmark name

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,23 @@
+product_name=$focusmark_productname
+
 echo Deploying into the $deployed_environment environment.
 npm install
 
 # Execute the SAM CLI Deploy command to upload the Lambdas to S3 and deploy them
-sam_stack_name=focusmark-"$deployed_environment"-sam-api-project
+sam_stack_name=$product_name-"$deployed_environment"-sam-api-project
 sam_template_file='template.sam'
-sam_s3_bucket_name=focusmark-$deployed_environment-s3-deployments
+sam_s3_bucket_name=$product_name-$deployed_environment-s3-deployments
 
 echo Deploying the $sam_stack_name stack.
 sam deploy \
   --template-file $sam_template_file \
   --stack-name $sam_stack_name \
-  --parameter-overrides TargetEnvironment=$deployed_environment \
   --s3-bucket $sam_s3_bucket_name \
   --s3-prefix focusmark-$deployed_environment-sam-api_project \
-  --capabilities CAPABILITY_NAMED_IAM
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+      TargetEnvironment=$deployed_environment \
+      ProductName=$product_name
 
 # Execute the CloudFormation template needed to map the API Gateway associated with the above SAM tempalte
 # to the existing custom domain deployed with the core infrastructure
@@ -25,4 +29,6 @@ aws cloudformation deploy \
   --template-file $cf_template_file \
   --stack-name $cf_stack_name \
   --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides TargetEnvironment=$deployed_environment
+  --parameter-overrides \
+      TargetEnvironment=$deployed_environment \
+      ProductName=$product_name
