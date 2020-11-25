623ca2cf58b2fe35cb2ec922af075ce8b79e2890
Scionwest <scionwest+github@gmail.com>
2020-11-25 06:01:30 +0000

    Removed DNS support from this repository. This now lives in aws-dns repo

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
+if [ -z "$deployed_environment" ]
+    then 
+        echo "\$deployed_environment environment variable is unset!"
+        echo "Aborting deployment."
+        exit
+fi
+
 product_name=$focusmark_productname
 
 echo Deploying into the $deployed_environment environment.
@@ -15,21 +22,6 @@ sam deploy \
   --stack-name $sam_stack_name \
   --s3-bucket $sam_s3_bucket_name \
   --s3-prefix focusmark-$deployed_environment-sam-api_project \
-  --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides \
-      TargetEnvironment=$deployed_environment \
-      ProductName=$product_name
-
-# Execute the CloudFormation template needed to map the API Gateway associated with the above SAM tempalte
-# to the existing custom domain deployed with the core infrastructure
-cf_stack_name=focusmark-"$deployed_environment"-cf-api-projectroute
-cf_template_file='domain-mapping.yaml'
-
-echo Deploying the $cf_stack_name stack.
-cfn-lint $cf_template_file
-aws cloudformation deploy \
-  --template-file $cf_template_file \
-  --stack-name $cf_stack_name \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
       TargetEnvironment=$deployed_environment \
